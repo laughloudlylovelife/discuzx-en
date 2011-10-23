dac84392252d1adf5c3e51bdf8c53711ee55163c
vvotintsev <vvotintsev@gmail.com>
2011-10-23 17:02:01 +0000

    Dzx2ML: Arabic LangPack (draft) uploaded.

---
+++ dzx2-langpack/ar/task/lang_invite.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_invite.php by Khalid Nahhal, http://www.ar-discuz.com
+ */
+
+$lang = array
+(
+	'invite_message'	=> '<p style="color:red;">نجاح باهر , انت بالفعل قمت بدعوة  {count} اصدقاء !</p><br>',//'<p style="color:red;">哇，厉害，您现在已经邀请了 {count} 个好友了。继续努力！</p><br>',
+	'invite_guide'	=> '<strong>اتبع الارشادات لاكمال هذه المهمة بنجاح:</strong>
+		<ul class="task">
+		<li>افتح في صفحة جديدة <a href="home.php?mod=spacecp&ac=invite" target="_blank">صفحة دعوة الاصدقاء</a>;</li>
+		<li>قم بدعوة اصدقاءك للانضمام الينا. استخدم الرسائل او البريد في ارسال رابط الدعوة;</li>
+		<li>يجب عليك دعوة 10 اصدقاء لاكمال هذه المهمة بنجاح.</li>
+		</ul>',
+
+);
+
+?>
