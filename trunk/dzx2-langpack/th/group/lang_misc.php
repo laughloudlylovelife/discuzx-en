bcd1e339faf7093d799d85d02d6c7fe555f2419f
vvotintsev@gmail.com <vvotintsev@gmail.com@ed102d2c-d53f-e219-56b2-8766b63ae0a5>
2011-11-23 13:54:20 +0000

    X2ML: Thai Language Pack just added.

---
+++ trunk/dzx2-langpack/th/group/lang_misc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**+++
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_misc.php 13309 2010-07-30 07:03:44Z liulanbo $
+ */
+
+$lang = array
+(
+	'group_normal_member'		=> 'สมาชิกทั่วไป',
+	'group_goaway'			=> 'ไล่ออกจาก{$_G[setting][navs][3][navname]}',
+	'group_demise_message_title'	=> "{$_G[setting][navs][3][navname]}{forum}ของคุณ ถูกโอนย้ายเรียบร้อย",
+	'group_demise_message_body'	=> "{$_G[setting][navs][3][navname]}{forum}ของฉัน ถูกโอนย้ายเรียบร้อย [url={siteurl}forum.php?mod=group&fid={fid}]คลิกที่นี่เพื่อดู{$_G[setting][navs][3][navname]}[/url]",
+	'group_join'			=> 'เข้าร่วม{$_G[setting][navs][3][navname]}{groupname}',
+);
+
