13e87a705cef9a00d4f87e9367047ecd0d577c02
vvotintsev <vvotintsev@gmail.com>
2011-12-22 12:09:29 +0000

    X2ML: All language packs copied into the X2ML branch.

---
+++ discuzx2.0ML/upload/source/language/ar/adv/lang_feed.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_feed.php by Khalid Nahhal, http://www.ar-discuz.com
+ */
+
+$lang = array
+(
+	'feed_name'	=> 'اعلان في اخبار الملف الشخصي',//'空间 动态广告',
+	'feed_desc'	=> 'اظهار الاعلان اعلى صفحة التغذية.',//'展现方式: 日志广告显示于动态的上方。',
+);
+
---
+++ discuzx2.0ML/upload/source/language/ar/task/lang_email.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_email.php by Khalid Nahhal, http://www.ar-discuz.com
+ */
+
+$lang = array
+(
+	'email_name'	=> 'مهمة تفعيل البريد الالكتروني',//'验证邮箱任务',
+	'email_desc'	=> 'اكتب بريدك , وتحقق منه وسوف تحصل على مكافأة.',//'验证邮箱获得相应的奖励。',
+	'email_view'	=> '<strong>اتبع الارشادادت لاكمال المهمة بنجاح:</strong>
+		<ul>
+		<li><a href="home.php?mod=spacecp&ac=profile&op=password" target="_blank">فتح اعددات الملف الشخصي</a>;</li>
+		<li>ضع بريدك الصحيح, ثم اضغط  "تحقق من البريد " ;</li>
+		<li>سيتم ارسال رسالة من النظام. سوف تستقبلها ثم افتحها وقم بالضغط على رابط التفعيل .</li>
+		</ul>',
+);
+
+?>
