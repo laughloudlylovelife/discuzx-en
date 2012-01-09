d9d93c79b584d8f9ce7748c04d0af1c49cadbaa2
vvotintsev <vvotintsev@gmail.com>
2012-01-09 17:39:47 +0000

    X2: French Pack: fr/forum/lang_dz_feed.php updated.

--- discuzx2.0ML/upload/source/language/fr/forum/lang_dz_feed.php
+++ discuzx2.0ML/upload/source/language/fr/forum/lang_dz_feed.php
@@ -5,53 +5,53 @@
  *      This is NOT a freeware, use is subject to license terms
  *
  *      $Id: lang_dz_feed.php by Valery Votintsev at sources.ru
- *	Traduction en French par Andre13
+  *	    Traduction en French par Andre13 - 26 -déc-2011- discuz-fr.fr
  */
 
 $lang = array
 (
-	'feed_thread_pin_title'		=> '{actor} thread <em>{subject}</em> was sticked by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 置顶了',
+	'feed_thread_pin_title'		=> '{actor} sujet <em>{subject}</em> a été scotché par {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 置顶了',
 
-	'feed_thread_digest_title'	=> '{actor} thread <em>{subject}</em> was digged by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 加精了',
+	'feed_thread_digest_title'	=> '{actor} sujet <em>{subject}</em> was digged by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 加精了',
 
-	'feed_thread_highlight_title'	=> '{actor} thread <em>{subject}</em> was highlighted by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 高亮了',
+	'feed_thread_highlight_title'	=> '{actor} sujet <em>{subject}</em> a été souligné par {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 高亮了',
 
-	'feed_thread_del_title'		=> '{actor} thread <em>{subject}</em> was deleted by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 删除了',
+	'feed_thread_del_title'		=> '{actor} sujet <em>{subject}</em> a été supprimé par {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 删除了',
 
-	'feed_thread_move_title'	=> '{actor} thread <em>{subject}</em> was moved by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 移动了',
+	'feed_thread_move_title'	=> '{actor} sujet <em>{subject}</em> a été déplacé par {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 移动了',
 
-	'feed_thread_views_title'	=> '{actor} thread <em>{subject}</em> was viewed more than {count} times',//'{actor} 发表的主题 {subject} 查看数超过了 {count}',
+	'feed_thread_views_title'	=> '{actor} sujet <em>{subject}</em> a été vu plus de {count} times',//'{actor} 发表的主题 {subject} 查看数超过了 {count}',
 
-	'feed_thread_replies_title'	=> '{actor} thread <em>{subject}</em> was replied more than {count} times',//'{actor} 发表的主题 {subject} 回复数超过了 {count}',
+	'feed_thread_replies_title'	=> '{actor} sujet <em>{subject}</em> a eût des réponses plus de {count} times',//'{actor} 发表的主题 {subject} 回复数超过了 {count}',
 
-	'feed_thread_rate_title'	=> '{actor} thread <em>{subject}</em> was rated more than {count}',//'{actor} 发表的主题 {subject} 评分超过了 {count}',
+	'feed_thread_rate_title'	=> '{actor} sujet <em>{subject}</em> a été évalué plus de {count}',//'{actor} 发表的主题 {subject} 评分超过了 {count}',
 
-	'feed_thread_magic_title'	=> '{actor} thread <em>{subject}</em> was used {magiccard} by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 使用了 {magiccard}',
+	'feed_thread_magic_title'	=> '{actor} sujet <em>{subject}</em> was used {magiccard} by {operater}',//'{actor} 发表的主题 {subject} 被 {operater} 使用了 {magiccard}',
 
-	'feed_thread_recommend_title'	=> '{actor} thread <em>{subject}</em> was recommeded by moderator',//'{actor} 发表的主题 {subject} 被版主推荐',
+	'feed_thread_recommend_title'	=> '{actor} sujet <em>{subject}</em> a été recommandé par le Modérateur',//'{actor} 发表的主题 {subject} 被版主推荐',
 
-	'feed_post_rate_title'		=> '{actor} post in <em>{thread}</em> was rated more than {count}',//'{actor} 在 {thread} 的回复评分超过了{count}',
+	'feed_post_rate_title'		=> '{actor} post dans <em>{thread}</em> a été évalué plus de {count}',//'{actor} 在 {thread} 的回复评分超过了{count}',
 
-	'feed_post_magic_title'		=> '{actor} reply in <em>{thread}</em> was used {magiccard} by {operater}',//'{actor} 在 {thread} 的回复被 {operater} 使用了 {magiccard}',
+	'feed_post_magic_title'		=> '{actor} répond au <em>{thread}</em> a été utilisé {magiccard} by {operater}',//'{actor} 在 {thread} 的回复被 {operater} 使用了 {magiccard}',
 
-	'feed_user_profile_title'	=> '{actor} updated personal profile',//'{actor} 更新了自己的个人资料',
+	'feed_user_profile_title'	=> '{actor} a Mis à jour son Profil Perso.',//'{actor} 更新了自己的个人资料',
 
-	'feed_user_usergroup_title'	=> '{actor} user group upgraded to {usergroup}',//'{actor} 的用户组升级为 {usergroup}',
+	'feed_user_usergroup_title'	=> '{actor} groupe Utilisateurs mis à niveau pour {usergroup}',//'{actor} 的用户组升级为 {usergroup}',
 
-	'feed_user_credit_title'	=> '{actor} total credits was reached {count}',//'{actor} 的总积分达到 {count}',
+	'feed_user_credit_title'	=> '{actor} total des crédits ont été atteint {count}',//'{actor} 的总积分达到 {count}',
 
-	'feed_user_threads_title'	=> '{actor} thread was reached {count}',//'{actor} 发表的主题数达到 {count}',
+	'feed_user_threads_title'	=> '{actor} fil de la discussion a été atteint {count}',//'{actor} 发表的主题数达到 {count}',
 
-	'feed_user_posts_title'		=> '{actor} post was reached {count}',//'{actor} 的发帖数达到 {count}',
+	'feed_user_posts_title'		=> '{actor} post a été atteint {count}',//'{actor} 的发帖数达到 {count}',
 
-	'feed_user_digest_title'	=> '{actor} digest was reached {count}',//'{actor} 的精华贴数达到 {count}',
+	'feed_user_digest_title'	=> '{actor} résumé a été atteint {count}',//'{actor} 的精华贴数达到 {count}',
 
-	'feed_user_medal_title'		=> '{actor} got the medal {medal}',//'{actor} 得到勋章 {medal} 奖励',
+	'feed_user_medal_title'		=> '{actor} obtient la médaille {medal}',//'{actor} 得到勋章 {medal} 奖励',
 
-	'feed_announce_title'		=> 'Announce: {subject}',//'公告: {subject}',
+	'feed_announce_title'		=> 'Annonce: {subject}',//'公告: {subject}',
 	'feed_announce_body'		=> '{message}',
 
-	'you'				=> 'You',//'您',
+	'you'				=> 'Vous',//'您',
 
 );
 
