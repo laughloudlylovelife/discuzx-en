d80651baee0e0d5f5369369b228794ffbd0e3414
vvotintsev <vvotintsev@gmail.com>
2012-07-12 17:56:07 +0000

    AR lang upgraded to 20120628

--- discuzx2.0ML/upload/source/discuz_version.php
+++ discuzx2.0ML/upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X2');
 	define('DISCUZ_RELEASE', '20120628');
-	define('DISCUZ_ML_REVISION', '395');
+	define('DISCUZ_ML_REVISION', '396');
 }
 
 ?>
